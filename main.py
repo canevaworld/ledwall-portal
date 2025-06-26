2ffe71e39380982b2f9ef7d080092a48dfa27239
canevaworld <jhonny.masotto03@icloud.com>
2025-06-26 21:15:44 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,7 @@
-python\nfrom fastapi import FastAPI\n\napp = FastAPI()\n\n@app.get(\"/\")\ndef read_root():\n return {\"status\": \"ok\", \"message\": \"LedWall portal online ✔\"}\n
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get("/")
+def read_root():
+    return {"status": "ok", "message": "LedWall portal online ✔"}
